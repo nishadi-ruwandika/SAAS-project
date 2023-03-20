dcc6c0b43caad7d43a37e40abb6d8532fce9c5f9
nishadi-ruwandika <118653265+nishadi-ruwandika@users.noreply.github.com>
2023-03-19 21:14:11 -0700

    Add files via upload

---
+++ saas-project/script.js
@@ -0,0 +1,17 @@
+const parentContainer =  document.querySelector('.sec-2-r-para');
+
+parentContainer.addEventListener('click', event=>{
+
+    const current = event.target;
+
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+
+    if(!isReadMoreBtn) return;
+
+    const currentText = event.target.parentNode.querySelector('.read-more-text');
+
+    currentText.classList.toggle('read-more-text--show');
+
+    current.textContent = current.textContent.includes('Read More') ? "Learn Less" : "Learn More";
+
+})
